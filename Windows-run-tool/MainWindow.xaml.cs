e061370583dcdb22f7081a9c1a830d5163406cb7
zti <zhaotianff@163.com>
2020-10-12 21:17:07 +0800

    temp commit

--- Windows-run-tool/MainWindow.xaml.cs
+++ Windows-run-tool/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Windows_run_tool.IO;
 using Windows_run_tool.Model;
+using Windows_run_tool.Reg;
 
 namespace Windows_run_tool
 {
@@ -22,6 +23,8 @@ namespace Windows_run_tool
     /// </summary>
     public partial class MainWindow : TianXiaTech.BlurWindow
     {
+        List<RunItem> runList = new List<RunItem>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -29,14 +32,78 @@ namespace Windows_run_tool
 
         private void BlurWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            var files = DirectoryExtension.GetFiles(@"C:\Windows\System32",".msc",".cpl");
+            LoadRunList();
+        }
 
-            var list = files.Select(x=>new RunItem() {Name = System.IO.Path.GetFileName(x),Path = x,Description = FileExtension.GetFileDescription(x) }).ToList();
+        private async void LoadRunList()
+        {
+            await LoadRegisterRunItem();
+        }
 
+        private async Task<List<RunItem>> LoadSystem32DirItemAsync()
+        {
+            var list = new List<RunItem>();
 
-     
+            try
+            {
+                await Task.Run(()=> {
+                    var windir = Environment.GetEnvironmentVariable("windir");
 
-            this.listview.ItemsSource = list;
+                    if (string.IsNullOrEmpty(windir))
+                    {
+                        windir = "C:\\Windows";
+                    }
+
+                    var sys32dir = System.IO.Path.Combine(windir, "System32");
+                    var files = DirectoryExtension.GetFiles(sys32dir, ".msc", ".cpl");
+                    list =  files.Select(x => new RunItem() { Name = System.IO.Path.GetFileName(x), Path = x, Description = FileExtension.GetFileDescription(x) }).ToList();
+                });
+
+                return list;
+            }
+            catch
+            {
+                return list;
+            }
+        }
+
+        private async Task<List<RunItem>> LoadRegisterRunItem()
+        {
+            var list = new List<RunItem>();
+
+            try
+            {
+                await Task.Run(() =>
+                {
+                    var appList1 = RegisterExtension.GetRegItem(Microsoft.Win32.Registry.LocalMachine, @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths");
+                    var appList2 = RegisterExtension.GetRegItem(Microsoft.Win32.Registry.ClassesRoot, @"Applications");
+
+                    foreach (var item in appList1)
+                    {
+                        var runitem = new RunItem();
+                        runitem.Name = item;
+                        var fullPath = RegisterExtension.GetRegValue(Microsoft.Win32.Registry.LocalMachine, @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\" + item, null);
+
+                        if (string.IsNullOrEmpty(fullPath))
+                            continue;
+
+                        fullPath = fullPath.Replace('"', ' ');
+
+                        if (System.IO.File.Exists(fullPath))
+                            runitem.Path = fullPath;
+                        else
+                            runitem.Path = System.IO.Path.Combine(fullPath, item);
+                        runitem.Description = FileExtension.GetFileDescription(runitem.Path);
+                        list.Add(runitem);
+                    }
+                });
+
+                return list;
+            }
+            catch
+            {
+                return list;
+            }
         }
     }
 }
--- Windows-run-tool/Reg/RegisterExtension.cs
+++ Windows-run-tool/Reg/RegisterExtension.cs
@@ -8,9 +8,30 @@ namespace Windows_run_tool.Reg
 {
     public static class RegisterExtension
     {
-        public static List<string> GetRegItem(string path)
+        public static List<string> GetRegItem(Microsoft.Win32.RegistryKey key,string path)
+        {            
+            try
+            {
+                var regKey = key.OpenSubKey(path);
+                return regKey.GetSubKeyNames().ToList();               
+            }
+            catch
+            {
+                return new List<string>();
+            }
+        }
+
+        public static string GetRegValue(Microsoft.Win32.RegistryKey key,string path,string name)
         {
-            return new List<string>();   
+            try
+            {
+                var regKey =  key.OpenSubKey(path);
+                return regKey.GetValue(name,"").ToString();
+            }
+            catch
+            {
+                return "";
+            }
         }
     }
 }
